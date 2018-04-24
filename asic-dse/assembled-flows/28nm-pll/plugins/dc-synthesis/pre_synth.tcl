d81cae9a3cd3c437a7cef8bb4dd68375a99f54cc
clt67 <clt67@cornell.edu>
2018-04-23 20:12:41 -0400

    [pll] small fix

--- asic-dse/assembled-flows/28nm-pll/plugins/dc-synthesis/pre_synth.tcl
+++ asic-dse/assembled-flows/28nm-pll/plugins/dc-synthesis/pre_synth.tcl
@@ -1,1 +1,7 @@
-set DC_SKIP_OPTIMIZE_NETLIST TRUE
+# NOTE: Tried skipping area optimization to prevent DC from optimizing
+# stuff away in the PLL that we need. But for some reason, if we skip area
+# optimization, then Innovus postroute dies saying the design is not
+# routed.
+
+#set DC_SKIP_OPTIMIZE_NETLIST TRUE
+
