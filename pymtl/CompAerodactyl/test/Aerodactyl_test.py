ff42cd0cf684062162270dbbe4e7b457284770b0
hawajkm <hawajkm@gmail.com>
2018-05-06 22:09:02 -0400

    [pymtl/Aerodactyl] Adding a unified file that import all tests

---
+++ pymtl/CompAerodactyl/test/Aerodactyl_test.py
@@ -0,0 +1,15 @@
+#=========================================================================
+# Aerodactyl_test.py
+#=========================================================================
+#
+# This file imports all tests that we have for this composition.
+# It will import tests for processor as well as tests for all host-enabled
+# component on the chip.
+#
+# For now, the following component has their tests included:
+#
+#   ASM, MDU
+#
+
+from Aerodactyl_asm_test import *
+from Aerodactyl_mdu_test import *
