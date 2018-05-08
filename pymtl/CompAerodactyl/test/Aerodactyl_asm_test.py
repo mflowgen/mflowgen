abe898daaf9019eed8fc41389218fa421dcdf6d7
Berkin Ilbeyi <berkin@csl.cornell.edu>
2018-05-08 12:59:23 -0400

    [Aerodactyl] Integrate bloom accelerator

--- pymtl/CompAerodactyl/test/Aerodactyl_asm_test.py
+++ pymtl/CompAerodactyl/test/Aerodactyl_asm_test.py
@@ -277,3 +277,35 @@ def test_sw( name, test, src_delay, sink_delay, mem_stall_prob,
             src_delay      = src_delay     , sink_delay  = sink_delay ,
             mem_stall_prob = mem_stall_prob, mem_latency =mem_latency )
 
+#-------------------------------------------------------------------------
+# bloom
+#-------------------------------------------------------------------------
+
+from bloom.test import inst_bloom
+
+@pytest.mark.parametrize(
+      "name, test,                              src_delay, sink_delay, mem_stall_prob, mem_latency",
+  **synthesize_testtable(
+    [
+      asm_test( inst_bloom.gen_basic_test     , 0        , 0         , 0             , 0           ) ,
+      asm_test( inst_bloom.gen_bypass_test    , 0        , 0         , 0             , 0           ) ,
+      asm_test( inst_bloom.gen_bypass_test    , 3        , 5         , 0.5           , 3           ) ,
+      asm_test( inst_bloom.gen_basic2_test    , 0        , 0         , 0             , 0           ) ,
+      asm_test( inst_bloom.gen_stream_test    , 0        , 0         , 0             , 0           ) ,
+    ]
+  )
+)
+def test_bloom( name, test, src_delay, sink_delay, mem_stall_prob,
+                mem_latency, dump_vcd, test_verilog ):
+
+  #               ctrlreg_msg    assembly_image   mdu_msgs icache_msgs  dcache
+  test_vector = [ "asm",         test,            [],      [],          [] ]
+
+  # Note: the bloom tests make use of one core only. This is because there
+  # is an interaction with the memory port, so we don't want interaction
+  # from other cores.
+  run_test( test_vector, dump_vcd, test_verilog,
+            src_delay      = src_delay     , sink_delay  = sink_delay ,
+            mem_stall_prob = mem_stall_prob, mem_latency =mem_latency,
+            only_one_core=True )
+
