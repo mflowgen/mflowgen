d05e270208b2bec822a31347d43dcbe93d587017
clt67 <clt67@cornell.edu>
2018-05-08 17:43:25 -0400

    add Butterfree

---
+++ pymtl/CompButterfree/test/HostButterfree_run_test.py
@@ -0,0 +1,38 @@
+#=========================================================================
+# HostButterfree_run_test.py
+#=========================================================================
+
+import pytest
+
+from pymtl                         import *
+from fpga                          import SwShim
+
+from Butterfree_harness            import run_test as run
+
+# Import designs
+from CompButterfree.Butterfree     import Butterfree
+from CompButterfree.HostButterfree import HostButterfree
+
+#-------------------------------------------------------------------------
+# Redefining run_test for Hosted version
+#-------------------------------------------------------------------------
+
+def run_test( test, dump_vcd, test_verilog,
+              src_delay=0, sink_delay=0, mem_stall_prob=0, mem_latency=0 ):
+
+  asynch_bitwidth = 8
+
+  dut             = Butterfree()
+  hwshim_and_dut  = HostButterfree( asynch_bitwidth )
+  swshim          = SwShim( dut, hwshim_and_dut, asynch_bitwidth,
+                                 dump_vcd, test_verilog )
+
+  # Set explicit name
+  swshim.explicit_modulename = swshim.__class__.__name__
+
+  num_cores       = 4
+  cacheline_nbits = 128
+
+  run( swshim, test, num_cores, cacheline_nbits,
+       dump_vcd, test_verilog, src_delay, sink_delay,
+       mem_stall_prob, mem_latency )
