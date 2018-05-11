267ac3c9feaa92478f213e22c0b8cb59af9b67e6
Tuan Ta <qtt2@cornell.edu>
2018-05-11 14:25:04 -0400

    [pymtl/Chansey] add icache coalescer to Chansey

--- pymtl/CompChansey/Chansey.py
+++ pymtl/CompChansey/Chansey.py
@@ -2,26 +2,27 @@
 # Chansey.py
 #=========================================================================
 
-from pymtl                   import *
-from pclib.ifcs              import InValRdyBundle, OutValRdyBundle
+from pymtl                      import *
+from pclib.ifcs                 import InValRdyBundle, OutValRdyBundle
 
 # BRGTC2 custom MemMsg modified for RISC-V 32
 
-from ifcs                    import CtrlRegMsg, MemMsg, MduMsg
+from ifcs                       import CtrlRegMsg, MemMsg, MduMsg
 
-from ctrlreg.CtrlReg         import CtrlReg
-from proc.ProcPRTL           import ProcPRTL
-from proc.NullXcelRTL        import NullXcelRTL
-from mdu.IntMulDivUnit       import IntMulDivUnit
-from instbuffer.InstBuffer   import InstBuffer
+from ctrlreg.CtrlReg            import CtrlReg
+from proc.ProcPRTL              import ProcPRTL
+from proc.NullXcelRTL           import NullXcelRTL
+from mdu.IntMulDivUnit          import IntMulDivUnit
+from instbuffer.InstBuffer      import InstBuffer
 
-from cache.BlockingCachePRTL import BlockingCachePRTL
+from cache.BlockingCachePRTL    import BlockingCachePRTL
 
-from networks.Funnel         import Funnel
-from networks.Router         import Router
-from adapters.HostAdapter    import HostAdapter
+from networks.Funnel            import Funnel
+from networks.Router            import Router
+from adapters.HostAdapter       import HostAdapter
 
-from bloom.BloomFilterXcel   import BloomFilterXcel
+from bloom.BloomFilterXcel      import BloomFilterXcel
+from mem_coalescer.MemCoalescer import MemCoalescer
 
 class Chansey( Model ):
 
@@ -95,8 +96,10 @@ class Chansey( Model ):
     s.icache         = BlockingCachePRTL( 0, wide_access = True )
     s.icache_adapter = HostAdapter( req=s.icache.cachereq, resp=s.icache.cacheresp )
 
-    s.net_icachereq  = Funnel( num_cores, s.cache_mem_ifc.req  )  # N L0is - to - 1 cache
-    s.net_icacheresp = Router( num_cores, s.cache_mem_ifc.resp )  # 1 cache - to - N L0is
+    # N L0is <-> icache_coalescer <-> 1 cache
+    s.icache_coalescer  = MemCoalescer( num_cores, s.cache_mem_ifc,
+                                        addr_nbits, cacheline_nbits,
+                                        mopaque_nbits )
 
     # Shared L1D
 
@@ -181,13 +184,13 @@ class Chansey( Model ):
 
     for i in xrange( num_cores ):
       s.connect_pairs(
-        # proc -> L0i -> net_icachereq
-        s.proc[i].imemreq,       s.l0i[i].buffreq,
-        s.l0i[i].memreq,         s.net_icachereq.in_[i],
+        # proc -> L0i -> icache_coalescer
+        s.proc[i].imemreq,            s.l0i[i].buffreq,
+        s.l0i[i].memreq,              s.icache_coalescer.reqs[i],
 
-        # net_icacheresp -> L0i -> proc
-        s.net_icacheresp.out[i], s.l0i[i].memresp,
-        s.l0i[i].buffresp,       s.proc[i].imemresp,
+        # icache_coalescer -> L0i -> proc
+        s.icache_coalescer.resps[i],  s.l0i[i].memresp,
+        s.l0i[i].buffresp,            s.proc[i].imemresp,
       )
 
     # nets <-> icache <-> imem
@@ -200,8 +203,8 @@ class Chansey( Model ):
       s.host_icachereq,   s.icache_adapter.hostreq,
       s.host_icacheresp,  s.icache_adapter.hostresp,
 
-      s.net_icachereq.out,  s.icache_adapter.realreq,
-      s.net_icacheresp.in_, s.icache_adapter.realresp,
+      s.icache_coalescer.memreq,  s.icache_adapter.realreq,
+      s.icache_coalescer.memresp, s.icache_adapter.realresp,
 
       s.icache_adapter.req,  s.icache.cachereq,
       s.icache_adapter.resp, s.icache.cacheresp,
