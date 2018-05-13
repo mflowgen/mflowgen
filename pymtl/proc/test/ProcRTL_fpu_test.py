032459a7214df7f79293d88c6762ed0ee23073a1
Berkin Ilbeyi <berkin@csl.cornell.edu>
2018-05-13 13:10:44 -0400

    [proc] add tests for fsub.s, fmul.s, fdiv.s

--- pymtl/proc/test/ProcRTL_fpu_test.py
+++ pymtl/proc/test/ProcRTL_fpu_test.py
@@ -55,3 +55,72 @@ def test_fadds_delays( dump_vcd, test_verilog ):
   run_test( ProcRTL, inst_fadds.gen_random_test, dump_vcd, test_verilog,
             src_delay=3, sink_delay=10, mem_stall_prob=0.5, mem_latency=3 )
 
+#-------------------------------------------------------------------------
+# fsub.s
+#-------------------------------------------------------------------------
+
+import inst_fsubs
+
+@pytest.mark.parametrize( "name,test", [
+  asm_test( inst_fsubs.gen_basic_test            ),
+  asm_test( inst_fsubs.gen_dest_dep_test         ),
+  asm_test( inst_fsubs.gen_src0_dep_test         ),
+  asm_test( inst_fsubs.gen_src1_dep_test         ),
+  asm_test( inst_fsubs.gen_srcs_dep_test         ),
+  asm_test( inst_fsubs.gen_srcs_dest_test        ),
+  asm_test( inst_fsubs.gen_value_test            ),
+  asm_test( inst_fsubs.gen_random_test           ),
+])
+def test_fsubs( name, test, dump_vcd, test_verilog ):
+  run_test( ProcRTL, test, dump_vcd, test_verilog )
+
+def test_fsubs_delays( dump_vcd, test_verilog ):
+  run_test( ProcRTL, inst_fsubs.gen_random_test, dump_vcd, test_verilog,
+            src_delay=3, sink_delay=10, mem_stall_prob=0.5, mem_latency=3 )
+
+#-------------------------------------------------------------------------
+# fmul.s
+#-------------------------------------------------------------------------
+
+import inst_fmuls
+
+@pytest.mark.parametrize( "name,test", [
+  asm_test( inst_fmuls.gen_basic_test            ),
+  asm_test( inst_fmuls.gen_dest_dep_test         ),
+  asm_test( inst_fmuls.gen_src0_dep_test         ),
+  asm_test( inst_fmuls.gen_src1_dep_test         ),
+  asm_test( inst_fmuls.gen_srcs_dep_test         ),
+  asm_test( inst_fmuls.gen_srcs_dest_test        ),
+  asm_test( inst_fmuls.gen_value_test            ),
+  asm_test( inst_fmuls.gen_random_test           ),
+])
+def test_fmuls( name, test, dump_vcd, test_verilog ):
+  run_test( ProcRTL, test, dump_vcd, test_verilog )
+
+def test_fmuls_delays( dump_vcd, test_verilog ):
+  run_test( ProcRTL, inst_fmuls.gen_random_test, dump_vcd, test_verilog,
+            src_delay=3, sink_delay=10, mem_stall_prob=0.5, mem_latency=3 )
+
+#-------------------------------------------------------------------------
+# fdiv.s
+#-------------------------------------------------------------------------
+
+import inst_fdivs
+
+@pytest.mark.parametrize( "name,test", [
+  asm_test( inst_fdivs.gen_basic_test            ),
+  asm_test( inst_fdivs.gen_dest_dep_test         ),
+  asm_test( inst_fdivs.gen_src0_dep_test         ),
+  asm_test( inst_fdivs.gen_src1_dep_test         ),
+  asm_test( inst_fdivs.gen_srcs_dep_test         ),
+  asm_test( inst_fdivs.gen_srcs_dest_test        ),
+  asm_test( inst_fdivs.gen_value_test            ),
+  asm_test( inst_fdivs.gen_random_test           ),
+])
+def test_fdivs( name, test, dump_vcd, test_verilog ):
+  run_test( ProcRTL, test, dump_vcd, test_verilog )
+
+def test_fdivs_delays( dump_vcd, test_verilog ):
+  run_test( ProcRTL, inst_fdivs.gen_random_test, dump_vcd, test_verilog,
+            src_delay=3, sink_delay=10, mem_stall_prob=0.5, mem_latency=3 )
+
