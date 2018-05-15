ef1143ddaae7d77e4baef49452ff60aaa60e37d8
clt67 <clt67@cornell.edu>
2018-05-15 11:07:39 -0400

    [tapein4] dc optimizations

--- asic-dse/assembled-flows/default/plugins/dc-synthesis/pre_synth.tcl
+++ asic-dse/assembled-flows/default/plugins/dc-synthesis/pre_synth.tcl
@@ -3,11 +3,31 @@
 #=========================================================================
 # This plug-in script is called before synthesis
 
-# Enable additional area optimizations
+# Enable additional area optimizations (skip false = enable optimizations)
 
-set DC_SKIP_OPTIMIZE_NETLIST true
+set DC_SKIP_OPTIMIZE_NETLIST false
 
 # Enable clock-gating
 
 set DC_GATE_CLOCK true
+
+# DC flatten effort
+#
+# - Effort 0: No auto-ungrouping / boundary optimizations (strict hierarchy)
+# - Effort 1: No auto-ungrouping / boundary optimizations
+#             DesignWare cells are ungrouped
+# - Effort 2: Enable auto-ungrouping / boundary optimizations
+#             DesignWare cells are ungrouped
+# - Effort 3: Everything ungrouped + level param for how deep to ungroup
+#
+# Note that even with boundary optimizations off, DC will still propagate
+# constants across the boundary, although this can be disabled with a
+# variable if we really wanted to disable it.
+
+set DC_FLATTEN_EFFORT 1
+
+# When boundary optimizations are off, set this variable to true to still
+# allow unconnected registers to be removed.
+
+set compile_optimize_unloaded_seq_logic_with_no_bound_opt true
 
