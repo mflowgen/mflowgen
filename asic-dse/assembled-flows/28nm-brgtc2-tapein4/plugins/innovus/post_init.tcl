7db7fa534c9db4988aef213cdd90f08affe9fb38
clt67 <clt67@cornell.edu>
2018-05-15 14:46:32 -0400

    [tapein4] add dont touches to balanced clk mux in innovus

--- asic-dse/assembled-flows/28nm-brgtc2-tapein4/plugins/innovus/post_init.tcl
+++ asic-dse/assembled-flows/28nm-brgtc2-tapein4/plugins/innovus/post_init.tcl
@@ -1,1 +1,22 @@
-../../../default/plugins/innovus/post_init.tcl
+#=========================================================================
+# post_init.tcl
+#=========================================================================
+# This plug-in script is called after the corresponding Innovus flow step
+#
+#-------------------------------------------------------------------------
+# Can be used for various floorplan related tasks, like:
+#              - Die/core boundary
+#              - placement of hard macros/blocks
+#              - power domain size and clearence surrounding to it
+#              - Placement and routing blockages in the floorplan
+#              - IO ring creation
+#              - PSO planning
+#-------------------------------------------------------------------------
+
+report_ports > $vars(rpt_dir)/$vars(step).ports.rpt
+
+# Don't touch the balanced clock mux built with hand-instantiated stdcells
+
+set_dont_touch clk_mux
+set_dont_touch clk_inv
+
