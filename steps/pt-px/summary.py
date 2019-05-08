3a0d702b0fdcd0a92d85b484c7762cdfcbcbdb4c
sr972 <sr972@cornell.edu>
2019-05-08 11:15:11 -0400

    Area, clockP, Power, Slack

--- steps/pt-px/summary.py
+++ steps/pt-px/summary.py
@@ -1,4 +1,5 @@
 import json
+import gzip
 
 with open('reports/innovus/signoff.area.rpt', 'r') as fd:
   lines = fd.readlines()
@@ -7,8 +8,13 @@ area = lines[2].split()[3]
 with open('reports/pt-px/signoff.pwr.rpt', 'r') as fd:
   lines = fd.readlines()
 power = lines[30].split()[3]
-    
-data = { 'Area': area , 'Power': power}
+
+with gzip.open("reports/innovus/signoff_all.tarpt.gz", "rb") as fd:
+  lines = fd.readlines() 
+clk_p = lines[14].split()[3]
+slack = lines[18].split()[3]
+
+data = { 'Area': area , 'Power': power , 'ClockP': clk_p , 'Slack': slack} 
     
 with open( 'reports/pt-px/summary.json', 'w' ) as fd:
   json.dump( data, fd, sort_keys=True, indent=4,
