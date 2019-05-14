87c216e641455d30053686dbf2448e1a694047c7
clt67 <clt67@cornell.edu>
2019-05-13 22:30:57 -0400

    [innovus] clock port pins are default floorplanned center-left

--- plugins/innovus/floorplan.tcl
+++ plugins/innovus/floorplan.tcl
@@ -17,13 +17,25 @@ planDesign
 
 # Take all ports and split into halves
 
-set all_ports       [dbGet top.terms.name]
+set all_ports       [dbGet top.terms.name -v *clk*]
 
 set num_ports       [llength $all_ports]
 set half_ports_idx  [expr $num_ports / 2]
 
 set pins_left_half  [lrange $all_ports 0               [expr $half_ports_idx - 1]]
 set pins_right_half [lrange $all_ports $half_ports_idx [expr $num_ports - 1]     ]
+
+# Take all clock ports and place them center-left
+
+set clock_ports     [dbGet top.terms.name *clk*]
+set half_left_idx   [expr [llength $pins_left_half] / 2]
+
+if { $clock_ports != 0 } {
+  for {set i 0} {$i < [llength $clock_ports]} {incr i} {
+    set pins_left_half \
+      [linsert $pins_left_half $half_left_idx [lindex $clock_ports $i]]
+  }
+}
 
 # Spread the pins evenly across the left and right sides of the block
 
