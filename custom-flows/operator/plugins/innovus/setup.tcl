565c5a7b7004b29f0d7894b82a3da8b6e0006f57
clt67 <clt67@cornell.edu>
2019-05-21 18:14:34 -0400

    [operator] limit innovus to 4 parallel jobs

--- custom-flows/operator/plugins/innovus/setup.tcl
+++ custom-flows/operator/plugins/innovus/setup.tcl
@@ -1,1 +1,17 @@
-../../../../plugins/innovus/setup.tcl
+#=========================================================================
+# setup.tcl
+#=========================================================================
+# This design-specific setup.tcl can overwrite any variable in the
+# default innovus-flowsetup "setup.tcl" script
+#
+# Author : Christopher Torng
+# Date   : March 26, 2018
+
+# Reduced-effort flow that sacrifices timing to iterate more quickly
+
+set vars(reduced_effort_flow) false
+
+# Multithreading (and maybe distributed processing)
+
+set vars(local_cpus) 4
+
