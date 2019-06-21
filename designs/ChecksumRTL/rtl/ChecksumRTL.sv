f0afc8b0c8669495e71189340df3ef0c23e895a6
pp482 <pp482@cornell.edu>
2019-06-20 22:20:24 -0400

    Remove design SV

--- designs/ChecksumRTL/rtl/ChecksumRTL.sv
+++
@@ -1,8 +0,0 @@
-//------------------------------------------------------------------------
-// ChecksumRTL.sv
-//------------------------------------------------------------------------
-// This is the file that should be overwritten with PyMTL genenrated
-// SystemVerilog of the checksum unit. You will need to copy ChecksumRTL.sv
-// generated during Task 2.6 to overwrite this file. After making sure
-// this file has the checksum unit to be pushed through the ASIC flow, you
-// might also want to check that outputs/design.v indeed points to this file.
