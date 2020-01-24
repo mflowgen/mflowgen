489e2e46fafad19eda5171c99b9cea2dad3e4a8c
Alex Carsello <alex.carsello@gmail.com>
2020-01-24 10:51:45 -0800

    Split apart read_design and ptpx scripts

--- steps/synopsys-ptpx-genlibdb/START.tcl
+++ steps/synopsys-ptpx-genlibdb/START.tcl
@@ -33,6 +33,7 @@ if {[ info exists env(order) ] && $::env(order) != "None" } {
 } else {
   # Default order of scripts
   set order "
+    read_design.tcl
     ptpx.tcl
   "
 }
---
+++ steps/synopsys-ptpx-genlibdb/scripts/read_design.tcl
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------
+# Designer interface
+#-------------------------------------------------------------------------
+# Source the designer interface script, which sets up variables from the
+# build system, sets up ASIC design kit variables, etc.
+
+source -echo -verbose designer_interface.tcl
+
+#-------------------------------------------------------------------------
+# Setup
+#-------------------------------------------------------------------------
+
+# Set up paths and libraries
+
+set_app_var search_path      ". $ptpx_additional_search_path $search_path"
+set_app_var target_library   $ptpx_target_libraries
+set_app_var link_library     "* $ptpx_target_libraries"
+
+#-------------------------------------------------------------------------
+# Read design
+#-------------------------------------------------------------------------
+
+# Read and link the design
+
+read_verilog   $ptpx_gl_netlist
+current_design $ptpx_design_name
+
+link_design
+
+# Read in the SDC and parasitics
+
+read_sdc -echo $ptpx_sdc
+read_parasitics -format spef $ptpx_spef
