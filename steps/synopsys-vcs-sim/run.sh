95bef6553a4b65c903a160a4530743753d45811b
Teguh Hofstee <thofstee@stanford.edu>
2020-02-24 23:50:30 -0800

    update synopsys vcs sim

---
+++ steps/synopsys-vcs-sim/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+ARGS="-R -sverilog -timescale=1ns/1ps"
+ARGS="$ARGS -hsopt"
+
+ARGS="$ARGS -top $testbench_name"
+ARGS="$ARGS inputs/adk/stdcells.v"
+ARGS="$ARGS +vcs+dumpvars+outputs/design.vpd"
+
+for f in inputs/*.v; do
+    [ -e "$f" ] || continue
+    ARGS="$ARGS $f"
+done
+
+for f in inputs/*.sv; do
+    [ -e "$f" ] || continue
+    ARGS="$ARGS $f"
+done
+
+if [ -f "inputs/design.args" ]; then
+    ARGS="$ARGS -f inputs/design.args"
+fi
+
+vcs $ARGS
