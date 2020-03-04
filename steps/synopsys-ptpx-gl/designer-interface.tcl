b544d2495a3435277088df7eeb6afa20cf66a882
Christopher Torng <ctorng@stanford.edu>
2020-03-03 22:41:47 -0800

    [ptpx] add gl power step

---
+++ steps/synopsys-ptpx-gl/designer-interface.tcl
@@ -0,0 +1,48 @@
+#=========================================================================
+# designer_interface.tcl
+#=========================================================================
+# The designer_interface.tcl file is the first script run by PTPX (see the
+# top of ptpx.tcl) and sets up ASIC design kit variables and inputs.
+#
+# Author : Christopher Torng
+# Date   : May 20, 2019
+
+set ptpx_design_name        $::env(design_name)
+
+#-------------------------------------------------------------------------
+# Libraries
+#-------------------------------------------------------------------------
+
+set adk_dir                       inputs/adk
+
+set ptpx_additional_search_path   $adk_dir
+set ptpx_target_libraries         stdcells.db
+
+set ptpx_extra_link_libraries     [join "
+                                      [glob -nocomplain inputs/*.db]
+                                      [glob -nocomplain inputs/adk/*.db]
+                                  "]
+
+#-------------------------------------------------------------------------
+# Inputs
+#-------------------------------------------------------------------------
+
+set ptpx_gl_netlist         inputs/design.vcs.v
+set ptpx_sdc                inputs/design.pt.sdc
+set ptpx_spef               inputs/design.spef.gz
+set ptpx_saif               inputs/run.saif
+
+# The strip path must be defined!
+#
+#   export strip_path = th/design_foo
+#
+# There must _not_ be any quotes, or read_saif will fail. This fails:
+#
+#   export strip_path = "th/design_foo"
+#
+
+set ptpx_strip_path         $::env(strip_path)
+
+puts "done"
+
+
---
+++ steps/synopsys-ptpx-gl/ptpx.tcl
@@ -0,0 +1,100 @@
+#=========================================================================
+# ptpx.tcl
+#=========================================================================
+# Use Synopsys PrimeTime to run power analysis
+#
+# - Gate-level power analysis
+# - Averaged power analysis
+#
+# Requires:
+#
+# - *.v    -- gate-level netlist
+# - *.saif -- switching activity dump from gate-level simulation
+# - *.sdc  -- constraints (e.g., create_clock) from PnR
+# - *.spef -- parasitics from PnR
+#
+# Author : Christopher Torng
+# Date   : May 20, 2019
+#
+
+#-------------------------------------------------------------------------
+# Designer interface
+#-------------------------------------------------------------------------
+# Source the designer interface script, which sets up ASIC design kit
+# variables and inputs.
+
+source -echo -verbose designer_interface.tcl
+
+#-------------------------------------------------------------------------
+# Setup
+#-------------------------------------------------------------------------
+
+# Set up paths and libraries
+
+set_app_var search_path      ". $ptpx_additional_search_path $search_path"
+set_app_var target_library   $ptpx_target_libraries
+set_app_var link_library     [join "
+                               *
+                               $ptpx_target_libraries
+                               $ptpx_extra_link_libraries
+                             "]
+
+# Set up power analysis
+
+set_app_var power_enable_analysis true
+set_app_var power_analysis_mode   averaged
+
+set_app_var report_default_significant_digits 3
+
+#-------------------------------------------------------------------------
+# Read design
+#-------------------------------------------------------------------------
+
+# Read and link the design
+
+read_verilog   $ptpx_gl_netlist
+current_design $ptpx_design_name
+
+link_design
+
+# Read in the SDC and parasitics
+
+read_sdc -echo $ptpx_sdc
+read_parasitics -format spef $ptpx_spef
+
+# Read in switching activity
+
+report_activity_file_check $ptpx_saif -strip_path $ptpx_strip_path \
+  > reports/$ptpx_design_name.activity.pre.rpt
+
+read_saif $ptpx_saif -strip_path $ptpx_strip_path
+
+# Checks
+
+check_constraints -verbose \
+  > reports/$ptpx_design_name.checkconstraints.rpt
+
+check_power \
+  > reports/$ptpx_design_name.checkpower.rpt
+
+#-------------------------------------------------------------------------
+# Power analysis
+#-------------------------------------------------------------------------
+
+update_power
+
+#-------------------------------------------------------------------------
+# Final reports
+#-------------------------------------------------------------------------
+
+report_switching_activity \
+  > reports/$ptpx_design_name.activity.post.rpt
+
+report_power -nosplit \
+  > reports/$ptpx_design_name.power.rpt
+
+report_power -nosplit -hierarchy \
+  > reports/$ptpx_design_name.power.hier.rpt
+
+exit
+
