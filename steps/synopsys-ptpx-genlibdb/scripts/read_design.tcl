227d7b7ec883867d586df4d49076eb09ca139338
Guénolé LALLEMENT <guenole@stanford.com>
2020-03-25 11:27:13 -0700

    [synopsys-ptpx-genlibdb] - Update script to check if spef files are existing This step can now be used after synthesis to generate lib files The spef files are no more mandatory as inputs of the step

--- steps/synopsys-ptpx-genlibdb/scripts/read_design.tcl
+++ steps/synopsys-ptpx-genlibdb/scripts/read_design.tcl
@@ -28,6 +28,18 @@ current_design $ptpx_design_name
 link_design
 
 # Read in the SDC and parasitics
+# Try to read the sdc constraints files
+if {[ file exists $ptpx_sdc ]} {
+    puts "\n  > Info: Sourcing $ptpx_sdc\"\n"
+    read_sdc -echo $ptpx_sdc
+}  else {
+    puts "\n  > Warn: No sdc constraint file found\"\n"
+}
 
-read_sdc -echo $ptpx_sdc
-read_parasitics -format spef $ptpx_spef
+# Try to read the sped parasitic files
+if {[ file exists $ptpx_spef ]} {
+    puts "\n  > Info: Sourcing $ptpx_sdc\"\n"
+    read_parasitics -format spef $ptpx_spef
+}  else {
+    puts "\n  > Warn: No spef parasitic file found\"\n"
+}
