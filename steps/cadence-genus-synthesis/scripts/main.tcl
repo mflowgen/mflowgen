9ffe7987d5aee08bd39f1a0f5190adb64265184f
steveri <steveri@steveri.com>
2020-08-28 10:07:45 -0700

    Fix library ordering problems in genus step

--- steps/cadence-genus-synthesis/scripts/main.tcl
+++ steps/cadence-genus-synthesis/scripts/main.tcl
@@ -17,20 +17,120 @@ if { $gate_clock == True } {
   set_attr lp_insert_clock_gating true
 }
  
-set_attr library    [join "
-                      [glob -nocomplain inputs/adk/*.lib]
-                      [glob -nocomplain inputs/*.lib]
-                    "]
+# ------------------------------------------------------------------------
+# (begin compiling library and lef lists)
+# ------------------------------------------------------------------------
 
-set_attr lef_library [join "
-                       inputs/adk/rtk-tech.lef
-                       [glob -nocomplain inputs/adk/*.lef]
-                       [glob -nocomplain inputs/*.lef]
-                     "]
+# OLD/BAD
+# set_attr library    [join "
+#                       [glob -nocomplain inputs/adk/*.lib]
+#                       [glob -nocomplain inputs/*.lib]
+#                     "]
+# 
+# set_attr lef_library [join "
+#                        inputs/adk/rtk-tech.lef
+#                        [glob -nocomplain inputs/adk/*.lef]
+#                        [glob -nocomplain inputs/*.lef]
+#                      "]
+
+########################################################################
+# Library sets
+# 
+# Steveri update Aug 2020: fixed library load ordering.
+# For consistency, using code similar to what I found in
+# existing step 'cadence-innovus-flowsetup/setup.tcl'
+#
+# Also, added "lsort" to "glob" for better determinacy.
+# 
+# FIXME This could maybe could/should be wrapped up as a separate common
+# script somewhere? For use by both genus and flowsetup etc?
+# Better: Could be an executable bash script in e.g. "adks/bin"...?
+
+global vars
+set vars(adk_dir) inputs/adk
+
+########################################################################
+# Typical-case libraries
+set vars(libs_typical,timing) \
+    [join "
+        $vars(adk_dir)/stdcells.lib
+        [lsort [glob -nocomplain $vars(adk_dir)/stdcells-lvt.lib]]
+        [lsort [glob -nocomplain $vars(adk_dir)/stdcells-ulvt.lib]]
+        [lsort [glob -nocomplain $vars(adk_dir)/stdcells-pm.lib]]
+        [lsort [glob -nocomplain $vars(adk_dir)/iocells.lib]]
+        [lsort [glob -nocomplain inputs/*tt*.lib]]
+        [lsort [glob -nocomplain inputs/*TT*.lib]]
+        "]
+puts "INFO: Found typical-typical libraries $vars(libs_typical,timing)"
+foreach L $vars(libs_typical,timing) { echo "L_TT    $L" }
+
+########################################################################
+# Best-case libraries
+# - Process: ff
+# - Voltage: highest
+# - Temperature: highest (temperature inversion at 28nm and below)
+# FIXME note this code repeats all the bc libraries in the list at
+# least twice, because of the extra '*-bc-*' pattern...
+
+if {[file exists $vars(adk_dir)/stdcells-bc.lib]} {
+    set vars(libs_bc,timing) \
+        [join "
+            $vars(adk_dir)/stdcells-bc.lib
+            [lsort [glob -nocomplain $vars(adk_dir)/stdcells-lvt-bc.lib]]
+            [lsort [glob -nocomplain $vars(adk_dir)/stdcells-ulvt-bc.lib]]
+            [lsort [glob -nocomplain $vars(adk_dir)/stdcells-pm-bc.lib]]
+            [lsort [glob -nocomplain $vars(adk_dir)/iocells-bc.lib]]
+            [lsort [glob -nocomplain $vars(adk_dir)/*-bc*.lib]]
+            [lsort [glob -nocomplain inputs/*ff*.lib]]
+            [lsort [glob -nocomplain inputs/*FF*.lib]]
+        "]
+  puts "INFO: Found fast-fast libraries $vars(libs_bc,timing)"
+  foreach L $vars(libs_bc,timing) { echo "L_FF    $L" }
+}
+
+########################################################################
+# Worst-case libraries
+# - Process: ss
+# - Voltage: lowest
+# - Temperature: lowest (temperature inversion at 28nm and below)
+# FIXME is there a reason this only looks for iocells???
+
+if {[file exists $vars(adk_dir)/stdcells-wc.lib]} {
+    set vars(libs_wc,timing) \
+        [join "
+            $vars(adk_dir)/stdcells-wc.lib
+            [lsort [glob -nocomplain $vars(adk_dir)/iocells-wc.lib]]
+            [lsort [glob -nocomplain inputs/*ss*.lib]]
+            [lsort [glob -nocomplain inputs/*SS*.lib]]
+      "]
+  puts "INFO: Found slow-slow libraries $vars(libs_wc,timing)"
+  foreach L $vars(libs_wc,timing) { echo "L_SS    $L" }
+}
+
+########################################################################
+# LEF files
+set vars(lef_files) \
+[join "
+    $vars(adk_dir)/rtk-tech.lef
+    $vars(adk_dir)/stdcells.lef
+    [lsort [glob -nocomplain $vars(adk_dir)/stdcells-pm.lef]]
+    [lsort [glob -nocomplain $vars(adk_dir)/*.lef]]
+    [lsort [glob -nocomplain inputs/*.lef]]
+"]
+
+puts "INFO: Found LEF files $vars(lef_files)"
+foreach L $vars(lef_files) { echo "LEF    $L" }
+
+# ------------------------------------------------------------------------
+# (done compiling library and lef lists)
+# ------------------------------------------------------------------------
+
+set_attr library     $vars(libs_typical,timing)
+set_attr lef_library $vars(lef_files)
 
 set_attr qrc_tech_file [list inputs/adk/pdk-typical-qrcTechFile]
 
-read_hdl -sv [glob -directory inputs -type f *.v *.sv]
+read_hdl -sv [lsort [glob -directory inputs -type f *.v *.sv]]
 elaborate $design_name
 
 source "inputs/adk/adk.tcl"
