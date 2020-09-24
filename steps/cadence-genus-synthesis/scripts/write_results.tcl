f72b31dd64400698e63967dc88aab54f2fdd5e32
Taeyoung Kong <kongty0329@gmail.com>
2020-09-24 16:02:39 -0700

    Dump sdf and spef

--- steps/cadence-genus-synthesis/scripts/write_results.tcl
+++ steps/cadence-genus-synthesis/scripts/write_results.tcl
@@ -8,6 +8,8 @@
 
 write_snapshot -directory results_syn -tag final
 write_design -innovus -basename results_syn/syn_out
+write_sdf > results_syn/syn_out.sdf
+write_spef > results_syn/syn_out.spef
 
 write_name_mapping
 
