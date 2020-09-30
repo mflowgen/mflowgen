d76fdc5d9aa2f1ceb20fe0cf96b9af3b56220736
Alex Carsello <alex.carsello@gmail.com>
2020-09-30 14:26:08 -0700

    Make merged gds second output of signoff

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
@@ -6,13 +6,17 @@
 # Author : Christopher Torng
 # Date   : March 26, 2018
 
+streamOut $vars(results_dir)/$vars(design).gds.gz \
+    -units 1000 \
+    -mapFile $vars(gds_layer_map)
+
 set merge_files \
     [concat \
         [glob -nocomplain inputs/adk/*.gds*] \
         [glob -nocomplain inputs/*.gds*] \
     ]
 
-streamOut $vars(results_dir)/$vars(design).gds.gz \
+streamOut $vars(results_dir)/$vars(design)_innovus_merged.gds \
     -units 1000 \
     -mapFile $vars(gds_layer_map) \
     -merge $merge_files
