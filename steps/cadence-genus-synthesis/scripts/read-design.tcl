840aa9380c244fabac9c2f4e6a03928385bafd45
Max Strange <46541870+mbstrange2@users.noreply.github.com>
2020-11-26 13:57:57 -0800

    Bring the naming constraints back (#89)
    * Bring the naming constraints back
    
    * Address @ctorng coding convention concerns

--- steps/cadence-genus-synthesis/scripts/read-design.tcl
+++ steps/cadence-genus-synthesis/scripts/read-design.tcl
@@ -5,5 +5,13 @@
 # Date   : September 28, 2020
 
 read_hdl -sv [lsort [glob -directory inputs -type f *.v *.sv]]
+
+# Prevent backslashes from being prepended to signal names...
+# this causes SAIF files to be invalid...needs to be before elaboration.
+
+set_attribute hdl_array_naming_style %s_%d
+set_attribute hdl_bus_wire_naming_style %s_%d
+set_attribute bit_blasted_port_style %s_%d /
+
 elaborate $design_name
 
