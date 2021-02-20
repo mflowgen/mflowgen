740d70f4b7bcba18a8bd8e103edfb82eaf42daae
steveri <steveri@steveri.com>
2021-02-19 17:16:13 -0800

    one working extend_step

--- mflowgen/components/graph.py
+++ mflowgen/components/graph.py
@@ -21,10 +21,8 @@ class Graph:
     s._edges_i = {}
     s._edges_o = {}
     s._steps   = {}
-    s._todo = {}  ;# Connections waiting to be made
-    s._extnodes  = []  ;# Extensions  waiting to be made
-
-
+    s._todo    = {}   ;# Connections waiting to be made
+    s._extnodes= [] ;# list of extension nodes
 
     # System paths to search for ADKs (i.e., analogous to python sys.path)
     #
@@ -844,19 +842,9 @@ ranksep=0.8;
 
       # Mark this step as an "extend" step
       self._extnodes.append(n.name)
-      if DBG: print(f'    EXTNODE addinf {n.name}')
-
-
       step = self._add_step_with_handle(frame, n, DBG)
       self._connect_successor_nodes(frame, n, DBG)
-
       if DBG: print("  DONE\n\n")
-
-
-
-#   # Just added a new node; see if we can do extensions
-#   def _check_todo_extend_list(self, frame, DBG=0):
-
 
 
 
@@ -944,10 +932,6 @@ ranksep=0.8;
           if DBG: print(f"    Add it to the todo list")
           self._todo[node_name].append(succ_name)
 
-# Ext_connect must happen *after* step=Step() def and *before* add_step()
-
-
-
   def _connect_from_to(self, frame, from_name, to_name, DBG=0):
       '''
       Given names for "from" and "to" nodes, try and connect the two.
@@ -964,10 +948,7 @@ ranksep=0.8;
       else:
         if from_name in list(self._extnodes):
           if DBG: print(f'    FOUND EXTNODE {from_name}')
-          #    init.extend_inputs( custom_init.all_outputs() )
           to_node.extend_inputs( from_node.all_outputs() )
-
-
 
         self.connect_by_name(from_node, to_node)
         if DBG: print(f'   CONNECTED {from_name} -> {to_name}')
@@ -984,9 +965,8 @@ ranksep=0.8;
         return True
       
 
-  def _findvar(self, frame, varname):
+  def _findvar(self, frame, varname, DBG=0):
     """Search given frame for local or global var with called 'varname'"""
-    DBG=1
     try:
       value = frame.f_locals[varname] ;# This will fail if local not exists
       print(f'    Found local var {varname}')
