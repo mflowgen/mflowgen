f166f228b629dbb92972eb296bf65d362a1afe11
steveri <steveri@steveri.com>
2021-02-21 09:32:22 -0800

    code cleanup and refactor

--- mflowgen/components/easysteps.py
+++ mflowgen/components/easysteps.py
@@ -1,11 +1,14 @@
+#=========================================================================
+# easysteps.py
+#=========================================================================
+# Author : S. Richardson
+# Date   : February, 2021
+
 import os
 import inspect
 
 from mflowgen.components.step import Step
-# from mflowgen.components.edge import Edge
-# from mflowgen.utils           import get_top_dir
 from mflowgen.utils           import ParseNodes
-
 
 def add_custom_steps(self, frame, nodelist_string, DBG=0):
     '''
@@ -35,42 +38,36 @@ def add_custom_steps(self, frame, nodelist_string, DBG=0):
     for n in nodes.node_array:
         if DBG: print(f"  Found '{n.name}' - '{n.step}' -> {n.successors}   ")
         step = self._add_step_with_handle(frame, n, is_default=False, DBG=DBG)
-        self._build_todo_list(frame, n, DBG)
-        if DBG: print('')
-
 
 def extend_steps(self, frame, nodelist_string, DBG=0 ):
-  """
+    '''
+    # Add 'extension' steps whose sole purpose is to add new inputs to an existing node.
+    #
     # EXAMPLE:
-          #   extend_steps("custom_init - custom-init -> init")
-          # Does this:
-          #    custom_init = Step( this_dir + '/custom-init')
-          #    # Add extra input edges to innovus steps that need custom tweaks
-          #    init.extend_inputs( custom_init.all_outputs() )
-          #    g.add_step( custom_init )
-          #    g.connect_by_name( custom_init,  init )
-    """
-  if DBG: print("Extending existing steps")
-  nodes=ParseNodes(nodelist_string)
-  # frame = inspect.stack()[1][0]
-  for n in nodes.node_array:
-    if DBG: print(f"  Found '{n.name}' - '{n.step}' -> {n.successors}   ")
-    self._extnodes.append(n.name)  ;# Mark this step as an "extend" step
-    step = self._add_step_with_handle(frame, n, is_default=False, DBG=DBG)
-    self._build_todo_list(frame, n, DBG)
-    if DBG: print('')
+    #   extend_steps("custom_init - custom-init -> init")
+    #
+    # Is shorthand for:
+    #    custom_init = Step( this_dir + '/custom-init')
+    #    # Add extra input edges to innovus steps that need custom tweaks
+    #    init.extend_inputs( custom_init.all_outputs() )
+    #    g.add_step( custom_init )
+    #    g.connect_by_name( custom_init,  init )
+    #
+    '''
+    if DBG: print("Extending existing steps")
+    nodes=ParseNodes(nodelist_string)
+    for n in nodes.node_array:
+        if DBG: print(f"  Found '{n.name}' - '{n.step}' -> {n.successors}   ")
+        self._extnodes.append(n.name)  ;# Mark this step as an "extend" step
+        step = self._add_step_with_handle(frame, n, is_default=False, DBG=DBG)
 
-        
 def add_default_steps(self, frame, nodelist_string, DBG=0):
     "Similar to 'add_custom_steps' but adds 'default=True' parm to Step() def"
     if DBG: print("Adding default steps")
-#     frame = inspect.stack()[1][0]
     nodes=ParseNodes(nodelist_string)
     for n in nodes.node_array:
-      if DBG: print(f"  Found '{n.name}' - '{n.step}' -> {n.successors}   ")
-      step = self._add_step_with_handle(frame, n, is_default=True, DBG=DBG)
-      self._build_todo_list(frame, n, DBG)
-      if DBG: print('')
+        if DBG: print(f"  Found '{n.name}' - '{n.step}' -> {n.successors}   ")
+        step = self._add_step_with_handle(frame, n, is_default=True, DBG=DBG)
 
 def _add_step_with_handle(self, frame, node, is_default, DBG=0):
       '''
@@ -86,8 +83,7 @@ def _add_step_with_handle(self, frame, node, is_default, DBG=0):
       #     rtl = Step( this_dir + '/../common/rtl' )
       #     g.add_step( rtl )
       #
-      # Also: after step is built, checks todo list to see if
-      # anyone is waiting to connect to this step.
+      # Also: after step is built, add successors to todo list for later processing
       '''
       stepname   = node.name
       stepdir    = node.step
@@ -112,78 +108,68 @@ def _add_step_with_handle(self, frame, node, is_default, DBG=0):
 
       # Add step to graph
       self.add_step(step)
-      return step
 
-def _build_todo_list(self, frame, node, DBG=0):
-      '''
-      Given a node containing a list of successors, check each successor
-      to see if it exists in the calling frame yet. If so, connect them; 
-      otherwise, add it to a todo-list for later.
-      '''
-      node_name = node.name
+      # Add successors to todo list
       for succ_name in node.successors:
-        if DBG: print(f"    Adding {node_name}->{succ_name} to todo list")
-        self._todo[node_name].append(succ_name)
+        if DBG: print(f"    Adding {stepname}->{succ_name} to todo list")
+        self._todo[stepname].append(succ_name)
+
+      if DBG: print('')
+      return step
 
 def connect_outstanding_nodes(self, frame, DBG=0):
     '''
     # construct.py should call this method after all steps have been built,
     # to clear out the todo list.
     '''
-#     frame = inspect.stack()[1][0]
     print("PROCESSING CONNECTIONS IN TODO LIST")
     for from_name in self._todo:
 
-      # Must make shallow copy b/c we may be deleting elements in situ
-      to_list = self._todo[from_name].copy() ;
-      for to_name in to_list:
-        if DBG: print(f"  CONNECTING {from_name} -> {to_name}")
-        self._connect_from_to(frame, from_name, to_name, DBG)
-        self._todo[from_name].remove(to_name)
-        # if DBG: print(f'    REMOVED from todo list: {from_name} -> {to_name}\n')
+        # Only global vars end up on the todo list, so 'from' node must be global
+        from_node = frame.f_globals[from_name]
 
-def _connect_from_to(self, frame, from_name, to_name, DBG=0):
-      '''
-      Given names for "from" and "to" nodes, try and connect the two.
-      If the "to" node does not exist (yet) in the given calling frame,
-      return "False".
-      '''
-      # Only global vars end up on the todo list, so 'from' node must be global
-      from_node = frame.f_globals[from_name]
-      to_node   = self._findvar(frame, to_name, DBG)
+        # Must make shallow copy b/c we may be deleting elements in situ
+        to_list = self._todo[from_name].copy() ;
 
-      # If it's an "extension" node, connect all "from" outputs as "to" inputs
-      if from_name in list(self._extnodes):
-        if DBG: print(f'    Extnode: connecting all outputs to dest node')
-        to_node.extend_inputs( from_node.all_outputs() )
+        for to_name in to_list:
+            if DBG: print(f"  CONNECTING {from_name} -> {to_name}")
 
-      # Connect "from" -> "to" nodes
-      self.connect_by_name(from_node, to_node)
-      # if DBG: print(f'    CONNECTED {from_name} -> {to_name}')
-      if DBG: print('')
+            # Don't know (yet) whether to_node is local or global to calling frame (construct.py)
+            to_node   = self._findvar(frame, to_name, DBG)
+
+            # If "from" is an "extension" node, connect all "from" outputs as "to" inputs
+            if from_name in list(self._extnodes):
+              if DBG: print(f'    Extnode: connecting all outputs to dest node')
+              to_node.extend_inputs( from_node.all_outputs() )
+
+            # Connect "from" -> "to" nodes
+            self.connect_by_name(from_node, to_node)
+            # if DBG: print(f'    CONNECTED {from_name} -> {to_name}')
+            if DBG: print('')
+
+            self._todo[from_name].remove(to_name)
+            # if DBG: print(f'    REMOVED from todo list: {from_name} -> {to_name}\n')
 
 
 def _findvar(self, frame, varname, DBG=0):
-    """Search given frame for local or global var with called 'varname'"""
+    "Search given frame for local or global var 'varname'"
+
+    # print(f"Look for varname '{varname}' among list of frame's locals")
     try:
-      value = frame.f_locals[varname] ;# This will fail if local not exists
-      if DBG: print(f"    Found local var '{varname}'")
-      return value
+        value = frame.f_locals[varname] ;# This will fail if local not exists
+        if DBG: print(f"    Found local var '{varname}'")
+        return value
     except: pass
 
-    # if DBG: print(f"    {varname} not local, is it global perchance? ", end='')
+    # print(f"    {varname} not local, is it global perchance? ", end='')
     try:
-      value = frame.f_globals[varname] ;# This will fail if global not exists
-      if DBG: print(f"    Found global var '{varname}'")
-      return value
+        value = frame.f_globals[varname] ;# This will fail if global not exists
+        if DBG: print(f"    Found global var '{varname}'")
+        return value
     except: pass
 
-#     if DBG: print('')
+    # Give up
     print(f"**ERROR Could not find '{varname}'"); exit(13)
-# 
-# 
-#     if DBG: print("\n    not global either; guess it's not plugged in yet")
-#     return None
 
 
 
