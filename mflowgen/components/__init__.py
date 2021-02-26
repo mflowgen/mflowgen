9ce117cea35488b45db284db115bf0e86865798d
steveri <steveri@steveri.com>
2021-02-26 13:53:36 -0800

    undo __init__ changes

--- mflowgen/components/__init__.py
+++ mflowgen/components/__init__.py
@@ -1,6 +1,4 @@
-# Note '__init__.py' not required for python 3.3 and above, see
-# https://docs.python.org/3/reference/import.html#regular-packages
-
 from mflowgen.components.graph import Graph
 from mflowgen.components.step  import Step
 from mflowgen.components.edge  import Edge
+
