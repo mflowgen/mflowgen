45fa2fca917fb42530be5abc7734b6a8e5cb0b20
steveri <steveri@steveri.com>
2021-03-01 14:14:43 -0800

    undo __init__ changes

--- mflowgen/utils/__init__.py
+++ mflowgen/utils/__init__.py
@@ -1,7 +1,4 @@
-# Note '__init__.py' not required for python 3.3 and above, see
-# https://docs.python.org/3/reference/import.html#regular-packages
-
 from mflowgen.utils.helpers import get_top_dir, get_files_in_dir
 from mflowgen.utils.helpers import bold, yellow, red, green
 from mflowgen.utils.helpers import read_yaml, write_yaml
-from mflowgen.utils.helpers import stamp
+
