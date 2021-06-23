6ccc0f624d4495432616bcc1ee444d08cc31e590
maximilian.koschay <mkoschay@web.de>
2021-06-23 15:03:27 +0200

    Made netlist outputs of synthesis and PnR nodes case-insensitive, e.g. for netgen-LVS

--- steps/cadence-innovus-signoff/scripts/main.tcl
+++ steps/cadence-innovus-signoff/scripts/main.tcl
@@ -6,6 +6,9 @@
 # Author : Christopher Torng
 # Date   : January 13, 2020
 
+# Change name rules to be case-insensitive
+update_names -nocase
+
 source -verbose innovus-foundation-flow/INNOVUS/run_signoff.tcl
 
 
