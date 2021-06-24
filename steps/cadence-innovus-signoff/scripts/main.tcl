a3c2b7d160877cdb5156d8a9e5e7e865cd9fe33b
Maximilian Koschay <mkoschay@web.de>
2021-06-24 13:59:25 +0200

    Made comments more descriptive

--- steps/cadence-innovus-signoff/scripts/main.tcl
+++ steps/cadence-innovus-signoff/scripts/main.tcl
@@ -6,7 +6,10 @@
 # Author : Christopher Torng
 # Date   : January 13, 2020
 
-# Change name rules to be case-insensitive
+# Change name rules to be case insensitive to guarantee unique names 
+# even with case insensitive tools 
+# ("N23" and "n23" becomes -> "N23" and "n23_<num>")
+
 update_names -nocase
 
 source -verbose innovus-foundation-flow/INNOVUS/run_signoff.tcl
