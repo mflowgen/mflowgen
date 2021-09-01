fa6c8b69f7b26217cbee8f15e0ed7d95b5c52b87
steveri <steveri@steveri.com>
2021-09-01 09:55:16 -0700

    move uniquify var-set to a better place

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/always-source.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/always-source.tcl
@@ -19,17 +19,3 @@ source $vars(adk_dir)/adk.tcl
 # there is always a reports directory.
 
 mkdir -p $vars(rpt_dir)
-
-# [08/2021] There seems to be a source of nondeterminism when
-# building e.g. garnet glb tile. Also see garnet issue
-# https://github.com/StanfordAHA/garnet/issues/803
-# 
-# 
-#   **WARN: (IMPECO-560): The netlist is not unique, because the module
-#   'Tile_PE_mux_logic_1_20' is instantiated multiple times. Make the
-#   netlist unique by running 'set init_design_uniquify 1' before
-#   loading the design to avoid the problem.
-#
-# So...
-
-set init_design_uniquify 1
--- steps/cadence-innovus-init/scripts/main.tcl
+++ steps/cadence-innovus-init/scripts/main.tcl
@@ -6,6 +6,7 @@
 # Author : Christopher Torng
 # Date   : January 13, 2020
 
+set init_design_uniquify 1
 source -verbose innovus-foundation-flow/INNOVUS/run_init.tcl
 
 
