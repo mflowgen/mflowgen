97e309e7e632e6ee3796266553c9297259da04ac
Alex Carsello <ajcars@stanford.edu>
2022-01-05 16:00:30 -0800

    Add command to prevent innovus from leaving 1x wide gaps

--- steps/cadence-innovus-place/scripts/setup-inst-gap.tcl
+++ steps/cadence-innovus-place/scripts/setup-inst-gap.tcl
@@ -12,6 +12,11 @@
 # smallest available filler cell is larger than one coreSite.
 
 if {[info exists ADK_MIN_INST_GAP]} {
-    setPlaceMode -place_detail_legalization_inst_gap $ADK_MIN_INST_GAP
+  setPlaceMode -place_detail_legalization_inst_gap $ADK_MIN_INST_GAP
+  # Without this setting, Innovus will still leave width=1 gaps in
+  # advanced technology nodes.
+  if {[expr $ADK_MIN_INST_GAP > 1]} {
+    setPlaceMode -place_detail_use_no_diffusion_one_site_filler false
+  }
 }
 
