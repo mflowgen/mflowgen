4b569ba93d84bd7d183756da86689ac2f712e05f
steveri <steveri@steveri.com>
2022-02-18 05:57:12 -0800

    new routing parm "stop_on_failed_routing"

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/route-design-check.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/route-design-check.tcl
@@ -4,7 +4,27 @@
 # This plug-in script replaces the default "routeDesign" command in
 # Innovus flow scripts, activated via this var in setup.tcl
 #   `set vars(route,route_design,replace_tcl) <path>/route-design-check.tcl`
-#
+
+# Author : Stephen Richardson
+# Update : February 17, 2022
+# Issue  : https://github.com/mflowgen/mflowgen/issues/119
+
+# Optionally FAIL routeDesign if it e.g. creates short circuits. Note
+# that this is the default behavior, but only for technologies below 20nm
+# [Innovus Text Command Reference v. 19.10 p. 3204:
+# "-drouteAutoStop is false by default for 20nm designs and below."]
+
+# ANECDOTE:  without "-drouteAutoStop", garnet Tile_PE route step failed
+# silently, and the error only surfaced much later when LVS failed, see
+# https://github.com/StanfordAHA/garnet/issues/833
+
+# Failure mode can now be controlled by way of an environment variable,
+# e.g. "export stop_on_failed_routing true"
+
+if {[info exists ::env(stop_on_failed_routing) {
+    setNanoRouteMode -drouteAutoStop $::env(stop_on_failed_routing)
+}
+
 # Author : Stephen Richardson
 # Date   : September 1, 2021
 
