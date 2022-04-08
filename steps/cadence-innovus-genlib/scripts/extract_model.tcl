e2ff3e028a45aa0250643210e38de58e2d01db4e
Alex Carsello <ajcars@stanford.edu>
2022-04-08 13:10:38 -0700

    Extract .lib files for all views and merge into a single .lib

--- steps/cadence-innovus-genlib/scripts/extract_model.tcl
+++ steps/cadence-innovus-genlib/scripts/extract_model.tcl
@@ -1,2 +1,11 @@
-do_extract_model design.lib -view analysis_default
+set lib_files {}
+
+foreach view $vars(analysis_views) {
+    set lib_name "${view}.lib"
+    lappend lib_files $lib_name
+    set_analysis_view -setup $view -hold $view
+    do_extract_model $lib_name -view $view
+}
+
+merge_model_timing -library $lib_files -modes $vars(analysis_views) -mode_group combined -outfile design.lib
 
