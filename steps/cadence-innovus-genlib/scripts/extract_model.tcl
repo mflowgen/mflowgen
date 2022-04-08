b834f951244e829bf76a0bfcef5448006c69adde
Alex Carsello <ajcars@stanford.edu>
2022-04-08 13:24:36 -0700

    get analysis views using innovus comman

--- steps/cadence-innovus-genlib/scripts/extract_model.tcl
+++ steps/cadence-innovus-genlib/scripts/extract_model.tcl
@@ -1,11 +1,11 @@
 set lib_files {}
 
-foreach view $vars(analysis_views) {
+foreach view [all_analysis_views] {
     set lib_name "${view}.lib"
     lappend lib_files $lib_name
     set_analysis_view -setup $view -hold $view
     do_extract_model $lib_name -view $view
 }
 
-merge_model_timing -library $lib_files -modes $vars(analysis_views) -mode_group combined -outfile design.lib
+merge_model_timing -library $lib_files -modes [all_analysis_views] -mode_group combined -outfile design.lib
 
