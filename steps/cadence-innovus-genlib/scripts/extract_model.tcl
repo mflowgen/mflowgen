cd89a659c8ea6b821677747bca0e9da7d33d77bc
Alex Carsello <ajcars@stanford.edu>
2022-04-11 15:25:43 -0700

    Handle case where there's only one view

--- steps/cadence-innovus-genlib/scripts/extract_model.tcl
+++ steps/cadence-innovus-genlib/scripts/extract_model.tcl
@@ -7,5 +7,11 @@ foreach view [all_analysis_views] {
     do_extract_model $lib_name -view $view
 }
 
-merge_model_timing -library $lib_files -modes [all_analysis_views] -mode_group combined -outfile design.lib
+# If we have more than one analysis view, merge them into a single design.lib
+if {[llength $lib_files] > 1} {
+    merge_model_timing -library $lib_files -modes [all_analysis_views] -mode_group combined -outfile design.lib
+} else {
+    mv $lib_files design.lib
+}
+
 
