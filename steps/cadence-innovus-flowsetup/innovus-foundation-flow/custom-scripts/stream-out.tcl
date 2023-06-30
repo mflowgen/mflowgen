bfbbc5f1129fb947668f0f73e00180a636904a87
Po-Han Chen <pohan@stanford.edu>
2023-06-30 14:32:06 -0700

    discard the default option to make the codes cleaner

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
@@ -6,6 +6,15 @@
 # Author : Christopher Torng
 # Date   : March 26, 2018
 
+if { [info exists ADK_DBU_PRECISION] } { 
+    set stream_out_units $ADK_DBU_PRECISION
+} else {
+    set stream_out_units 1000
+}
+
+streamOut $vars(results_dir)/$vars(design).gds.gz \
+    -units ${stream_out_units} \
+    -mapFile $vars(gds_layer_map)
 
 set merge_files \
     [concat \
@@ -13,34 +22,8 @@ set merge_files \
         [lsort [glob -nocomplain inputs/*.gds*]] \
     ]
 
-if { [info exists ADK_DBU_PRECISION] } { 
-    if { $ADK_DBU_PRECISION == "default" } {
-        streamOut $vars(results_dir)/$vars(design).gds.gz \
-            -mapFile $vars(gds_layer_map)
-
-        streamOut $vars(results_dir)/$vars(design)-merged.gds \
-            -mapFile $vars(gds_layer_map) \
-            -uniquifyCellNames \
-            -merge $merge_files
-    } else {
-        streamOut $vars(results_dir)/$vars(design).gds.gz \
-            -units $ADK_DBU_PRECISION \
-            -mapFile $vars(gds_layer_map)
-
-        streamOut $vars(results_dir)/$vars(design)-merged.gds \
-            -units $ADK_DBU_PRECISION \
-            -mapFile $vars(gds_layer_map) \
-            -uniquifyCellNames \
-            -merge $merge_files
-    }
-} else {
-    streamOut $vars(results_dir)/$vars(design).gds.gz \
-        -units 1000 \
-        -mapFile $vars(gds_layer_map)
-
-    streamOut $vars(results_dir)/$vars(design)-merged.gds \
-        -units 1000 \
-        -mapFile $vars(gds_layer_map) \
-        -uniquifyCellNames \
-        -merge $merge_files
-}
+streamOut $vars(results_dir)/$vars(design)-merged.gds \
+    -units ${stream_out_units} \
+    -mapFile $vars(gds_layer_map) \
+    -uniquifyCellNames \
+    -merge $merge_files
