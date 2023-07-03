226dc5302c5af1cae7f7e002da270b03e7d90944
Po-Han Chen <64796998+pohantw@users.noreply.github.com>
2023-07-02 19:09:34 -0700

    Make -units option configurable through ADK (#142)
    * support dbu setting from ADK, keep 1000 if no adk variable set

--- steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/stream-out.tcl
@@ -6,8 +6,14 @@
 # Author : Christopher Torng
 # Date   : March 26, 2018
 
+if { [info exists ADK_DBU_PRECISION] } { 
+    set stream_out_units $ADK_DBU_PRECISION
+} else {
+    set stream_out_units 1000
+}
+
 streamOut $vars(results_dir)/$vars(design).gds.gz \
-    -units 1000 \
+    -units ${stream_out_units} \
     -mapFile $vars(gds_layer_map)
 
 set merge_files \
@@ -17,9 +23,7 @@ set merge_files \
     ]
 
 streamOut $vars(results_dir)/$vars(design)-merged.gds \
-    -units 1000 \
+    -units ${stream_out_units} \
     -mapFile $vars(gds_layer_map) \
     -uniquifyCellNames \
     -merge $merge_files
-               
-
