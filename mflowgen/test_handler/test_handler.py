35b109201be5ff178989df0fd6d4dcd7e899eb31
Alex Carsello <ajcars@stanford.edu>
2023-10-09 14:26:33 -0700

    Initial test handler commit: Ensures that all attach points are valid and built

---
+++ mflowgen/test_handler/test_handler.py
@@ -0,0 +1,197 @@
+#=========================================================================
+# test_handler.py
+#=========================================================================
+# Handler for test-related commands
+#
+# Author : Alex Carsello
+# Date   : Oct. 5, 2023
+#
+
+import os
+import re
+import shutil
+import subprocess
+import sys
+import yaml
+import importlib.util
+import glob
+
+from datetime       import datetime
+
+from mflowgen.utils import bold, yellow
+from mflowgen.utils import read_yaml
+
+
+#-------------------------------------------------------------------------
+# Test Management
+#-------------------------------------------------------------------------
+
+class TestHandler:
+
+  def __init__( s ):
+    s.metadata_dir = '.mflowgen'
+  
+  #-----------------------------------------------------------------------
+  # helpers
+  #-----------------------------------------------------------------------
+
+  # get_step_data
+  #
+  # Gets the step data dict for a given index
+
+  def get_step_data( s, step_index ):
+    step_metadata_dir = glob.glob( f"{s.metadata_dir}/{step_index}-*" )
+    try:
+      step_metadata_dir = step_metadata_dir[0]
+      step_data = read_yaml( f"{step_metadata_dir}/configure.yml" )
+      return step_data
+    except IndexError:
+      print( f"Error: {step_index} is not a valid step id in this build directory" )
+      sys.exit( 1 )
+
+
+  #-----------------------------------------------------------------------
+  # launch
+  #-----------------------------------------------------------------------
+  # Dispatch function for commands
+  #
+
+  def launch( s, help_, step, attach_points ):
+
+    if help_:
+      s.launch_help()
+      return
+
+    s.launch_test( step, attach_points )
+
+  #-----------------------------------------------------------------------
+  # launch_test
+  #-----------------------------------------------------------------------
+
+  def launch_test( s, step, attach_points ):
+    yaml_path = './.mflowgen.yml'
+    # Get the graph object
+    try:
+      data = read_yaml( yaml_path )
+    except:
+      print( 'Error: Must run mflowgen test inside configured build directory' )
+      sys.exit( 1 )
+    
+    construct_path = data['construct']
+    # Import the graph for this design (copied from RunHandler)
+    c_dirname  = os.path.dirname( construct_path )
+    c_basename = os.path.splitext( os.path.basename( construct_path ) )[0]
+    mod_spec = importlib.util.spec_from_file_location(c_basename, construct_path)
+    graph_construct_mod = importlib.util.module_from_spec(mod_spec)
+    try:
+      mod_spec.loader.exec_module(graph_construct_mod)
+    except ModuleNotFoundError:
+      print()
+      print( bold( 'Error:' ), 'Could not open construct script at',
+                                      '"{}"'.format( construct_path ) )
+      print()
+      sys.exit( 1 )
+    try:
+      graph_construct_mod.construct
+    except AttributeError:
+      print()
+      print( bold( 'Error:' ), 'No module named "construct" in',
+                                      '"{}"'.format( construct_path ) )
+      print()
+      sys.exit( 1 )
+    
+    # Construct the graph
+    s._graph = graph_construct_mod.construct()
+
+    # Find specified step directory
+    step_under_test = s.get_step_data( step )
+    # Get test(s) we need to run
+    try:
+      tests = step_under_test[ 'tests' ]
+    except KeyError:
+      print( f"Step {step} has no tests in this graph." )
+      return
+    
+    step_metadata_dirs = glob.glob(f"{s.metadata_dir}/[0-9]*-*")
+      
+    # Loop over the tests to ensure that each provides a test graph
+    # and collect the attach points if they're not provided in the cli args.
+    attach_point_tags = {}
+    for test in tests:
+      try:
+        test_graph = test['test_graph']
+      except KeyError:
+        print(f"Error: No test_graph provided for step {step} test.")
+        sys.exit(1)
+      # If user didn't provide an attach point arg, we will use the attach
+      # points specified in the test, so we store all of the tags we need
+      # in a set
+      if not attach_points:
+        for ap in test['attach_points']:
+          attach_point_tags.add( ap )
+     
+    synth_step = None
+    ap_step_dict = {}
+    # Find a synth step since we need the sdc from this to run tests
+    for step_metadata_dir in step_metadata_dirs:
+      step_data = read_yaml( f"{step_metadata_dir}/configure.yml" )
+      try:
+        step_ap_tags = step_data['attach_point_tags']
+      except KeyError:
+        continue
+      if 'SYNTHESIS' in step_ap_tags:
+        synth_step = step_data[ 'build_id' ]
+      # If the user didn't provide an attach point arg, find the step that corresponds to
+      # Each attach point tag we stored in the previous step
+      if not attach_points:
+        for ap_tag in attach_point_tags:
+          if ap_tag in step_tags:
+            ap_step_dict[ap_tag] = step_data[ 'build_id' ]
+
+    if not synth_step:
+      print( 'Error: Graph must contain at least one step with the SYNTHESIS attach point tag ' \
+             'in order for tests to run.' )
+  
+    ap_list = attach_points 
+    if not ap_list:
+      ap_list = list( ap_step_dict.values() )
+       
+    # Ensure that every attach point is valid
+    for attach_point in ap_list:
+      ap_data = s.get_step_data( attach_point )
+      try:
+        ap_outputs = ap_data['outputs']
+      except KeyError:
+        print(f"Error: Step {attach_point} is not a valid test attach " \
+              f"point becase it does not have any outputs.")
+        sys.exit(1)
+      if 'design.checkpoint' not in ap_outputs:
+        print(f"Error: Step {attach_point} is not a valid test attach " \
+              f"point because it does not produce an Innovus checkpoint " \
+              f"(design.checkpoint)")
+        sys.exit(1)
+
+    subprocess.check_call( f"make {synth_step}".split(' ') )    
+    for attach_point in ap_list:
+      # Make attach point
+      subprocess.check_call( f"make {attach_point}".split(' ') )
+      # Run the tests at the attach point
+      for test in tests:
+        print( f"Running test at attach point {attach_point}" )
+    
+    
+
+
+  #-----------------------------------------------------------------------
+  # launch_help
+  #-----------------------------------------------------------------------
+
+  def launch_help( s ):
+    print()
+    print( bold( 'Test Options:' ) )
+    print()
+    print( bold( ' --step :' ),          'The step whose tests you wish to run'                          )
+    print( bold( ' --attach_points :' ), 'Comma-separated list of attach points (locations to run test)' )
+    print()
+
+
