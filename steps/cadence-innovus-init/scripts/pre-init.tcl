a526d13f2181d01469c2cc155b63dcee82ec2343
Po-Han Chen <pohan@stanford.edu>
2023-10-17 14:49:02 -0700

    Fix time unit bug

--- steps/cadence-innovus-init/scripts/pre-init.tcl
+++ steps/cadence-innovus-init/scripts/pre-init.tcl
@@ -12,3 +12,13 @@ if {[file exists $vars(adk_dir)/pdk-qrc-lef.map]} {
 
 # Start Innovus in an assign-free flow
 set_db init_no_new_assigns true
+
+# (Hack) Set the Library Unit to match the sdc
+#    Intel (without a notice) changed the time unit from ps to ns in the 
+# standard cell libraries, while leaving other collateral libraries in ps.
+# For Genus, it chooses the finest time unit (ps) when generating SDCs.
+# For Innovus, it chooses the time unit of the main library (ns).
+# Because SDC is unit-less, Innovus will interpret the SDC using ns.
+# To avoid this issue, we set the library unit to match the SDC.
+# There might be some other way to do this, but this is the easiest.
+setLibraryUnit -time 1ps -cap 1fF
