048e07b4c32984d2c5e6adf422318f7b3ea25de8
Alex Carsello <ajcars@stanford.edu>
2024-03-10 17:22:48 -0700

    add custom script to set placement mode to floorplan mode for express testing flows

---
+++ steps/cadence-innovus-flowsetup/innovus-foundation-flow/custom-scripts/pre_place_fp_mode.tcl
@@ -0,0 +1,11 @@
+#=========================================================================
+# pre_place_fp_mode.tcl
+#=========================================================================
+# These pre/post tcl scripts are called before and after this Innovus
+# Foundation Flow step.
+#
+# Author : Alex Carsello
+# Date   : March 10, 2024
+#
+
+setPlaceMode -place_design_floorplan_mode true
