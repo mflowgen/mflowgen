6d9c6094419e656d3f242f61bb9eec1ed22b7742
norabarlow <kathleen.feng0@gmail.com>
2024-07-12 12:13:41 -0700

    Update cadence-innovus-place to take in list of cells to be padded

--- steps/cadence-innovus-place/scripts/setup-cellpad.tcl
+++ steps/cadence-innovus-place/scripts/setup-cellpad.tcl
@@ -11,8 +11,10 @@
 # placed. This leaves room for setup/hold fixing buffers to be placed next
 # to them later on.
 
-if {[info exists ADK_CELLS_TO_BE_PADDED]} {
-    specifyCellPad $ADK_CELLS_TO_BE_PADDED $::env(cell_padding)
+if { [info exists ADK_CELLS_TO_BE_PADDED] && $::env(padding) == "True" } {
+    foreach c $ADK_CELLS_TO_BE_PADDED {
+        specifyCellPad $c $::env(cell_padding)
+    }
 } else {
     specifyCellPad *DFF* $::env(cell_padding)
 }
