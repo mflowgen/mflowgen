89e2b731f931fac68967961a96a288d0d983761d
ctorng <ctorng@usc.edu>
2025-09-01 01:07:21 -0700

    [cts] better reporting

--- steps/cadence-innovus-cts/scripts/reporting.tcl
+++ steps/cadence-innovus-cts/scripts/reporting.tcl
@@ -4,6 +4,8 @@
 # Author : Christopher Torng
 # Date   : March 26, 2018
 
-report_ccopt_clock_trees -filename $vars(rpt_dir)/$vars(step).clock_trees.rpt
+report_ccopt_clock_trees -list_special_pins -filename $vars(rpt_dir)/$vars(step).clock_trees.rpt
 report_ccopt_skew_groups -filename $vars(rpt_dir)/$vars(step).skew_groups.rpt
+
+report_ccopt_clock_tree_structure -show_sinks -expand_generated_clock_trees independently -file $vars(rpt_dir)/$vars(step).structure.rpt
 
