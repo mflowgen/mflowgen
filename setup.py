adefbc5c52eb11b78b66c558fb0d467dc93b635d
Alex Carsello <alex.carsello@gmail.com>
2025-11-03 20:36:25 -0800

    Remove steps from exclude list in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
   #
 
   packages = find_packages(
-    exclude = [ 'adks', 'designs', 'docs', 'requirements', 'steps', 'nodes' ],
+    exclude = [ 'adks', 'designs', 'docs', 'requirements', 'nodes' ],
   ) + ['.'],
 
   # BSD 3-Clause License:
