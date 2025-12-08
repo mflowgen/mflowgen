aee0e5d640638bd38b44007d7258828eef66c641
ctorng <ctorng@usc.edu>
2025-12-07 16:56:29 -0800

    [innovus] add default commands for end caps to init node

--- nodes/cadence-innovus-init/scripts/add-endcaps-welltaps.tcl
+++ nodes/cadence-innovus-init/scripts/add-endcaps-welltaps.tcl
@@ -4,6 +4,21 @@
 # Author : Christopher Torng
 # Date   : March 4, 2020
 
+# Add end caps if ADK contains end caps
+
+if {   [info exists ADK_END_CAP_CELL_LEFT]
+    && [expr {$ADK_END_CAP_CELL_LEFT ne ""}]
+    && [info exists ADK_END_CAP_CELL_RIGHT]
+    && [expr {$ADK_END_CAP_CELL_RIGHT ne ""}] } {
+  # The -rightEdge option is for precap (left edge of core rows)
+  # The -leftEdge option is for postcap (right edge of core rows)
+  setEndCapMode -rightEdge $ADK_END_CAP_CELL_LEFT
+  setEndCapMode -leftEdge  $ADK_END_CAP_CELL_RIGHT
+  addEndCap     -prefix    ENDCAP
+} else {
+  echo "Warning: mflowgen skipping end cap insertion because none found in ADK"
+}
+
 # Add well taps if ADK contains well taps
 
 if {[info exists ADK_WELL_TAP_CELL] && [expr {$ADK_WELL_TAP_CELL ne ""}]} {
